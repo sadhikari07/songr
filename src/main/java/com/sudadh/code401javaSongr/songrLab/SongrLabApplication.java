fdf932132d28491a7594bf1eb3362aab3e21540b
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-06-12 06:42:28 -0700

    Repo created

---
+++ src/main/java/com/sudadh/code401javaSongr/songrLab/SongrLabApplication.java
@@ -0,0 +1,13 @@
+package com.sudadh.code401javaSongr.songrLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrLabApplication.class, args);
+	}
+
+}
